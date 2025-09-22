12d1f1076a52dab7141893c7cf482a0568a47ed4
Junaid Akram <makram.bese22seecs@seecs.edu.pk>
2025-09-23 00:09:05 +0500

    Add Comments Section

--- src/app/@model/compose/post/page.tsx
+++ src/app/@model/compose/post/page.tsx
@@ -24,7 +24,7 @@ const PostModal = () => {
         <div className="py-8 flex gap-4">
           <div className="relative w-10 h-10 rounded-full overflow-hidden">
             <Image
-              path="public/general/avatar.png"
+              path="public/general/avatar.jpg"
               alt="Lama Dev"
               w={100}
               h={100}
